30407c7612472e472c6dc6facb7f4da7bb5bf537
jobran <jobran41@gmail.com>
2018-06-03 14:53:53 +0100

    add scss

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import './index.css';
+
 import App from './components/App';
 import store from './store';
 import registerServiceWorker from './registerServiceWorker';
+
+import './index.css';
 
 ReactDOM.render(
   <Provider store={store}>
